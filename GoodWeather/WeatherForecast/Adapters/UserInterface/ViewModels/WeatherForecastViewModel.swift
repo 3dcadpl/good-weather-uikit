a553807a0bca0f55a495ce07e640da569276a296
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-05-25 08:20:07 +0200

    refactorings

--- GoodWeather/WeatherForecast/Adapters/UserInterface/ViewModels/WeatherForecastViewModel.swift
+++ GoodWeather/WeatherForecast/Adapters/UserInterface/ViewModels/WeatherForecastViewModel.swift
@@ -4,6 +4,7 @@ struct WeatherForecastViewModel {
     
     let forecast: [DayForecastViewModel] = []
     
+    private let icons = ["01d": "sun.max.fill", "02d": "cloud.sun.fill", "03d": "cloud.fill", "04d": "smoke.fill", "09d": "cloud.rain.fill", "10d": "cloud.sun.rain.fill", "11d": "cloud.sun.bolt.fill", "13d": "snow", "50d": "cloud.fog.fill"]
     private let weatherProvider: WeatherProvider
     private let dateformatter = DateFormatter()
     
